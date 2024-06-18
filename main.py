0dcbdb23ff291b6a6f5aef84e23a74b76a54bf03
SuprLazr <ericrc87@gmail.com>
2024-06-18 10:42:54 -0400

    used dict.get() to make the code less verbose than solution

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     word_count = get_word_count(text)
     print(f"{get_word_count(text)} words found in the document")
     char_count = get_char_count(text)
+    sort_char_count(char_count)
 
 def get_word_count(string):
     words = string.split()
@@ -13,12 +14,16 @@ def get_char_count(text):
     lower_text = text.lower()
     char_count = {}
     for letter in lower_text:
-        if letter in char_count:
-            char_count[letter] += 1
-        else:
-            char_count.update({letter : 1})
+        char_count[letter] = char_count.get(letter, 0) + 1
+        #if letter in char_count:
+         #   char_count[letter] += 1
+        #else:
+         #   char_count.update({letter : 1})
     print(char_count)
     return char_count
+
+def sort_char_count(dict):
+    pass    
 
 def get_book(path):
     with open(path) as f:
